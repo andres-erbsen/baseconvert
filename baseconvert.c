4b4aaed1cc3d90cb2a7831d99d1595f6e0d09de2
Andres Erbsen <andres.erbsen@gmail.com>
2012-08-20 11:46:25 +0300

    Fix several bugs

--- baseconvert.c
+++ baseconvert.c
@@ -8,7 +8,7 @@
 #include "tommath.h"
 typedef unsigned int uint;
 
-/* gcc -O3 baseconvert.c -lm -ltommath -o baseconvert */
+/* gcc -O3 -DEXECUTABLE baseconvert.c -lm -ltommath -o baseconvert */
 
 
 #define USAGE "Usage: baseconvert IN_DIGITS_FILE OUT_DIGITS_FILE\n\
@@ -19,15 +19,15 @@ Example: echo -n 15 | ./baseconvert decimal.txt base2.txt # gives 1111"
 /** all shortcuts available using 32-bit unsigned integers **/
 /* BIGGER_GROUP[i] digits of base with BIGGER_MAX_DIGIT[i] can be converted to */
 /* SMALLER_GROUP[i] digits if base SMALLER_MAX_DIGIT[i] or vice versa */
-/* for l in zip(*filter(lambda l: int(l[0])**int(l[1]) < 2**32, (re.findall('[0-9]+',s) for s in open('shortcuts.txt').read().splitlines()))): print('{'+', '.join([str(int(i)-1) for i in l]),'};\n') */
+/* for l in zip(*filter(lambda l: int(l[0])**int(l[1]) < 2**32, (re.findall('[0-9]+',s) for s in open('shortcuts.txt').read().splitlines()))): print('{'+', '.join(l),'};\n') and substracted 1 from radix to get maxdigit to fit in uint8*/
 #define N_TRICKS 47
 static const uint16_t BIGGER_MAX_DIGIT[N_TRICKS] = {3, 7, 7, 8, 15, 15, 15, 24, 26, 26, 31, 31, 31, 31, 35, 48, 63, 63, 63, 63, 63, 80, 80, 80, 99, 120, 124, 124, 127, 127, 127, 127, 143, 168, 195, 215, 215, 224, 242, 242, 242, 242, 255, 255, 255, 255, 255 };
 
-static const uint16_t BIGGER_GROUP[N_TRICKS] = {0, 0, 1, 0, 0, 0, 2, 0, 0, 1, 0, 1, 2, 3, 0, 0, 0, 0, 0, 1, 4, 0, 0, 2, 0, 0, 0, 1, 0, 1, 2, 3, 0, 0, 0, 0, 1, 0, 0, 1, 2, 3, 0, 0, 2, 0, 2 };
+static const uint16_t BIGGER_GROUP[N_TRICKS] = {1,1,2,1,1,1,3,1,1,2,1,2,3,4,1,1,1,1,1,2,5,1,1,3,1,1,1,2,1,2,3,4,1,1,1,1,2,1,1,2,3,4,1,1,3,1,3};
 
 static const uint16_t SMALLER_MAX_DIGIT[N_TRICKS] = {1, 1, 3, 2, 1, 3, 7, 4, 2, 8, 1, 3, 7, 15, 5, 6, 1, 3, 7, 15, 31, 2, 8, 26, 9, 10, 4, 24, 1, 3, 7, 15, 11, 12, 13, 5, 35, 14, 2, 8, 26, 80, 1, 3, 7, 15, 63 };
 
-static const uint16_t SMALLER_GROUP[N_TRICKS] = {1, 2, 2, 1, 3, 1, 3, 1, 2, 2, 4, 4, 4, 4, 1, 1, 5, 2, 1, 2, 5, 3, 1, 3, 1, 1, 2, 2, 6, 6, 6, 6, 1, 1, 1, 2, 2, 1, 4, 4, 4, 4, 7, 3, 7, 1, 3 };
+static const uint16_t SMALLER_GROUP[N_TRICKS] = {2,3,3,2,4,2,4,2,3,3,5,5,5,5,2,2,6,3,2,3,6,4,2,4,2,2,3,3,7,7,7,7,2,2,2,3,3,2,5,5,5,5,8,4,8,2,4};
 
 
 static void die (const char * format, ...) {
@@ -108,29 +108,33 @@ void _baseconvert_group_size_uint32( uint8_t in_max_digit
 
 /* These are macros because they will be reused for uint64 shortcuts */
 #define SHAKE() do { \
-    if (acc || gotsomething) { /* omit leading 0 groups */ \
-        for (j = out_group - 1;  j >= 0;  j--)  { \
-            s[j] = out_digits[acc % out_radix]; \
-            acc /= out_radix; \
-            if (gotsomething == 0 && acc == 0 ) { \
-                memmove(s,s+j,out_group - j); /* delete leading 0s */ \
-                s -= j; /* out_group is added later */ \
-                break; \
-            } \
-        } \
-        gotsomething = 1; \
-        s += out_group; \
+    /*fprintf(stderr," -> %u -> ",acc);*/ \
+    group = nextgroup; \
+    nextgroup += out_group_size; \
+    s = nextgroup - 1; \
+    while ( (acc || group != out_chars) && s >= group )  { \
+        *s = out_digits[acc % out_radix]; \
+        acc /= out_radix; \
+        s--; \
     } \
+    s++; /* now s >= group, could have been s = group - 1 otherwise */ \
+    /* omit leading 0 groups */ \
+    if (s > group) { \
+        memmove(group, s, nextgroup - s); \
+        nextgroup = group + (nextgroup - s); \
+    } \
+	/*fwrite(group, nextgroup - group, 1, stderr); fwrite("\n", 1, 1, stderr);*/ \
 } while(0);
 
 #define STEP(val) do { \
+    /*fprintf(stderr,"%d,",val);*/ \
     acc *= in_radix; \
     acc += val; \
     stepsleft--; \
     if (stepsleft == 0) { \
         SHAKE(); \
         assert(acc == 0); \
-        stepsleft = in_group; \
+        stepsleft = in_group_size; \
     } \
 } while (0);
 
@@ -139,25 +143,30 @@ uint _baseconvert_group_uint32( uint8_t in_max_digit
                               , uint8_t* in_value /* NOT digits, reverse lookup */
                               , char* in_chars
                               , uint  in_len
-                              , uint8_t in_group
+                              , uint8_t in_group_size
                               , uint8_t out_max_digit
                               , char* out_digits
                               , char* out_chars
                               , uint  out_len 
-                              , uint8_t out_group)
+                              , uint8_t out_group_size)
 {
     /* fprintf(stderr,"groupwise conversion!\n"); */
     unsigned long in_radix = in_max_digit+1, out_radix = out_max_digit+1;
-    uint8_t stepsleft = in_group;
-    char gotsomething=0;
-    char* s = out_chars;
+    unsigned char stepsleft = in_group_size;
+    char *nextgroup = out_chars, *group = 0, *s = 0;
     uint32_t acc = 0;
-    int i, j;
+    uint i;
+    /* fprintf(stderr,"%u %u\n",in_group_size, out_group_size);*/
 
-    for (i=0; i<(in_group-in_len%in_group)%in_group; i++) STEP(0);
+    /* pretend leading 0s */
+    for (i=0; i<(in_group_size-in_len%in_group_size)%in_group_size; i++) STEP(0); 
     for (i=0; i<in_len; i++) STEP( in_value[ ord(in_chars[i]) ] );
-    if (stepsleft != out_group) SHAKE();
-    return s - out_chars - out_group; /* s is `out_group` ahead of end */
+    if (stepsleft != in_group_size) SHAKE();
+    if (nextgroup == out_chars) {
+        *nextgroup = *out_digits;
+        nextgroup++;
+    }
+    return nextgroup - out_chars;
 }
 
 /* TODO: groupwise conversions for which state does not fit in uint32 */
@@ -188,6 +197,7 @@ uint baseconvert( uint8_t in_max_digit
                  , char* out_chars
                  , uint  out_len )
 {
+    /* fprintf(stderr,"baseconvert(%d, [...], [...], %d, %d, [...] [...], %d)\n", in_max_digit, in_len, out_max_digit, out_len); */
     uint i;
     /* digit -> value lookup table */
     uint8_t in_value[256];
@@ -207,13 +217,14 @@ uint baseconvert( uint8_t in_max_digit
 }
 
 uint baseconvert_targetlen(uint8_t in_max_digit, uint8_t out_max_digit, uint in_len) {
-    return 1 + in_len * log(in_max_digit+1)/log(out_max_digit+1) * 1.0000001;
+    return 1 + in_len * log(in_max_digit+1)/log(out_max_digit+1) * 1.0000002;
 }
 
 /* Read all that there is from a file handle */
 uint freadall(char** target, FILE* fh) {
     uint bufsize = 1024;
     char *buf = malloc(bufsize);
+    if (buf == NULL) return 0;
     uint len = 0;
     while (1) {	
         if ( (len = len + fread(buf+len,1,bufsize-len,stdin)) == bufsize ) {
@@ -228,7 +239,7 @@ uint freadall(char** target, FILE* fh) {
     return len;
 }
 
-
+#ifdef EXECUTABLE
 int main(int argc, char** argv) {
     unsigned long in_radix=0, out_radix=0;
     char *in_digits=NULL, *out_digits=NULL;
@@ -280,8 +291,10 @@ int main(int argc, char** argv) {
     }
     uint out_len = baseconvert_targetlen(in_max_digit, out_max_digit, in_len);
     char* converted = malloc(out_len);
+    if (converted == NULL) die("Failed to allocate memory.");
     out_len = baseconvert(in_max_digit, in_digits, stdinbytes, in_len,
                          out_max_digit, out_digits, converted, out_len);
 	if (fwrite(converted, out_len, 1, stdout) != 1) perror("Writing final output");
     return 0;
 }
+#endif
